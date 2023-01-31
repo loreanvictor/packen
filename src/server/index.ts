0fcf895699ccd9b37d0c55f226e0cc2de7515338
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2023-01-31 21:49:52 +0100

    fix exports

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,4 @@
+export * from '../iso/bundle'
 export * from './processor'
 export * from './write'
 export * from './pack'
--- src/server/test/exports.test.ts
+++ src/server/test/exports.test.ts
@@ -1,10 +1,11 @@
 import {
-  BareImportOptions, bareImport, Processor,
+  Bundle, BareImportOptions, bareImport, Processor,
   serialize, write
 } from '../'
 
 
 test('everything is exported', () => {
+  expect(Bundle).toBeDefined()
   expect(bareImport).toBeDefined()
   expect(<BareImportOptions>{}).toBeDefined()
   expect(<Processor>{}).toBeDefined()
